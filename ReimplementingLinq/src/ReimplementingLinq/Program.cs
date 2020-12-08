d0082e8a427ab7c92ceaf60757b112a376432945
omlbucket <omelianai@gmail.com>
2020-12-09 00:24:05 +0200

    Changed the code structure Added new solution for the deeper dive into linq by reimplementing them using TDD

---
+++ ReimplementingLinq/src/ReimplementingLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReimplementingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets create some nasty linqs!");
+        }
+    }
+}
