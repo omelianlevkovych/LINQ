a829e17b6f919e74f60c8b1dcb9f989daf2d0ab5
omlbucket <omelianai@gmail.com>
2020-12-12 00:02:53 +0200

    Implemented the linq Where operator with learning notes as documentation

---
+++ ReimplementingLinq/src/ReimplementingLinq.Tests/Utilities/ThrowingEnumerable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ReimplementingLinq.Tests.Utilities
+{
+    /// <summary>
+    /// The class responsible for verifying that linq operator is deferred.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class ThrowingExceptionEnumerable<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// The methods throws <see cref="InvalidOperationException"/>.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new InvalidOperationException();
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// The method which checks that the given <see cref="deferredFunction"/> actually uses deferred execution.
+        /// When the function just call itself it should not throw an exception. But, when using the result
+        /// by calling <see cref="GetEnumerator"/> and than GetNext() methods should throws the <see cref="InvalidOperationException"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The deferred function source type.</typeparam>
+        /// <typeparam name="TResult">The deferred function result type.</typeparam>
+        /// <param name="deferredFunction">The deferred function (unit of work under the test).</param>
+        public static void AssertDeferred<TSource,TResult>(
+            Func<IEnumerable<TSource>, IEnumerable<TResult>> deferredFunction)
+        {
+            var source = new ThrowingExceptionEnumerable<TSource>();
+            
+            // Does not throw any exception here, because GetEnumerator() method is not yet used.
+            var result = deferredFunction(source);
+
+            // Does not throw InvalidOperationException even here, despite the fact that we retrieve the enumerator.
+            using var iterator = result.GetEnumerator();
+
+            Assert.Throws<InvalidOperationException>(() => iterator.MoveNext());
+        }
+    }
+}
---
+++ ReimplementingLinq/src/ReimplementingLinq/Operators/Where.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReimplementingLinq.Operators
+{
+    /// <summary>
+    /// The class which contains all rewritten LINQ, in this file Where operator.
+    /// </summary>
+    public static partial class Enumerable
+    {
+        /// <summary>
+        /// The Where operator validation part.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>The <see cref="IEnumerable{T}"/></returns>
+        public static IEnumerable<TSource> Where<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return WhereImplementation(source, predicate);
+        }
+
+        /// <summary>
+        /// The Where operator implementation part.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>The <see cref="IEnumerable{T}"/></returns>
+        public static IEnumerable<TSource> WhereImplementation<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The Where operator with index, validation part.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>The <see cref="IEnumerable{T}"/></returns>
+        public static IEnumerable<TSource> Where<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, bool> predicate)
+        {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return WhereImplementation(source, predicate);
+        }
+
+        /// <summary>
+        /// The Where operator with index, implementation part.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>The <see cref="IEnumerable{T}"/></returns>
+        public static IEnumerable<TSource> WhereImplementation<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, int, bool> predicate)
+        {
+            var index = 0;
+            foreach (var item in source)
+            {
+                if (predicate(item, index))
+                {
+                    yield return item;
+                }
+
+                ++index;
+            }
+        }
+
+        private static IEnumerable<TSource> WrongWhere<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            // Differed execution so no of this validation will word, do to the fact we do not iterate throw source in our tests.
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException();
+            }
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
